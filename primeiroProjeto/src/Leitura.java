b39239e97aff8bc99b983efc2705bf27da8ba2fa
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-02-06 12:28:11 -0300

    usando Scanner

---
+++ primeiroProjeto/src/Leitura.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+
+        System.out.println("Qual ano de lançamento");
+        int anoDeLancamento = leitura.nextInt();
+    }
+}
