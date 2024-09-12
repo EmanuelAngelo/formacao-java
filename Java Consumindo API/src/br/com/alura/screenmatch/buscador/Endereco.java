65c8c86baf1eaa0bface5a2b3d143d596a7754c5
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-09-12 20:24:59 -0300

    testando api viacep

---
+++ Java Consumindo API/src/br/com/alura/screenmatch/buscador/Endereco.java
@@ -0,0 +1,5 @@
+package br.com.alura.screenmatch.buscador;
+
+public record Endereco(String logradouro, String cep, String localidade,
+                       String Complemento, String uf, String bairro) {
+}
