ee0e7e24c9e523e7a5f48a222c09528e0737ba5b
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-09-12 19:53:09 -0300

    salvando em json arquivo

--- Java Consumindo API/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ Java Consumindo API/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,10 +1,12 @@
 package br.com.alura.screenmatch.modelos;
 
-import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoExcepetion;
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -16,13 +18,15 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
-    public Titulo(TitutlOmdb meuTituloOmdb) {
+    public Titulo(TituloOmdb meuTituloOmdb) {
         this.nome = meuTituloOmdb.title();
-        if (meuTituloOmdb.year().length() > 4){
-            throw new ErroDeConversaoDeAnoExcepetion("Não consegui connverter o ano, porque tem mais de 04 caracateres.");
+
+        if(meuTituloOmdb.year().length() > 4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano " +
+                    "porque tem mais de 04 caracteres.");
         }
         this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
-        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
     }
 
     public String getNome() {
@@ -77,13 +81,13 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public int compareTo(Titulo outroTitulo) {
-
         return this.getNome().compareTo(outroTitulo.getNome());
     }
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento + "," + "duracao=" + duracaoEmMinutos + " min";
+        return "(nome = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento + "," +
+                " duração = " + duracaoEmMinutos + ")";
     }
 }
